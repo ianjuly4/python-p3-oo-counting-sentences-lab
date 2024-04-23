c280cfb88905eac8a27736065031c9307d377862
Ian July <ianjuly4@gmail.com>
2024-04-23 01:37:46 -0600

    counting sentences lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,43 @@
 #!/usr/bin/env python3
-
+import re
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print('The value must be a string.')
+
+    def is_sentence(self):
+        if self.value.endswith("."):
+            return True
+        else:
+            return False
+
+    def is_question(self):
+        if self.value.endswith("?"):
+            return True
+        else:
+            return False
+        
+    def is_exclamation(self):
+        if self.value.endswith("!"):
+            return True
+        else:
+            return False
+        
+    def count_sentences(self):
+        if not self.value:
+            return 0
+        sentences = re.split(r'[.!?]\s', self.value)
+        return len(sentences)
+    
+string = MyString()
+string.value = "one. two. three?"
+print(string.count_sentences())
